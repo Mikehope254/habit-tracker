8026633488a4f12a7c40bceb34e8f16413e2d1bc
Mikehope254 <mikehope1738@gmail.com>
2025-03-24 22:01:53 +0300

    routes defined

---
+++ backend/routes/routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  getHabit,
+  createhabit,
+  updateHabit,
+  deleteHabit,
+} from "../controllers/controllers.js";
+
+const router = express.Router();
+
+router.get("/", getHabit);
+router.post("/", createhabit);
+router.put("/:id", updateHabit);
+router.delete("/:id", deleteHabit);
+
+export default router;
